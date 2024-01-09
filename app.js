633bea9cd40b081352e749ba769cc22f3fe98778
DhaniCG <dhanigamer777@gmail.com>
2024-01-09 18:35:00 +0700

    Change nodemailer port to 25

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cors());
 
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: 465,
+    port: 25,
     auth: {
         user: "dhani@dhanidesigns.com",
         pass: process.env.PASSWORD,
